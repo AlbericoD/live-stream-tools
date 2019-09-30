737f9121ed95b1865bb760e7f996f567fa96cb0d
AlbericoD <a.diasbarreto@icloud.com>
2019-09-30 15:44:27 -0300

    alterações: - alteração no metodo de renderizar mensagens

--- frontend/chat-ao-vivo/index.js
+++ frontend/chat-ao-vivo/index.js
@@ -1,31 +1,32 @@
 //cliente escutando as mensagens do socket io
+const QDT_MAXIMA_DE_MENSAGENS = 5;
 let chat = io.connect('http://localhost:8080');
-
+let containerDoChat = document.querySelector('.chat');
 let mensagens = [];
 
+const templatesBadges = () =>
+  `<img src="https://static-cdn.jtvnw.net/badges/v1/5527c58c-fb7d-422d-b71b-f309dcb85cc1/1" >`;
+
+const templateMensagem = ({ badge, nome, color, msg }) =>
+  `<div class="container-mensagem">
+  <span class="badge">${templatesBadges(badge)}</span>
+    <span class="nome-espectador" style="color:${color}" >${nome}</span><span class="doispontos">:</span>&nbsp;
+    <p class="mensagem">${msg}</p>
+  </div>`;
+
 const inserirMensagem = conteudo => {
-  mensagens.push(conteudo);
-  if (mensagens.length > 5) mensagens.shift();
+  let mensagem = templateMensagem(conteudo);
+  mensagens.push(mensagem);
+  if (mensagens.length > QDT_MAXIMA_DE_MENSAGENS) mensagens.shift();
 };
 
-const ordernarMensagens = () => {
-  for (let index = 0; index < 5; index++) {
-    let conteudoMensagem = document.querySelector(`.mensagem-${index + 1}`);
-    conteudoMensagem.textContent = mensagens[index].msg;
-    conteudoMensagem.style.color = 'white';
-
-    let badgeEspectador = document.querySelector(`.badge-${index + 1}`);
-    let nomeEspectador = document.querySelector(`.nome-${index + 1}`);
-
-    nomeEspectador.textContent = mensagens[index].nome;
-    nomeEspectador.style.color = mensagens[index].color;
-
-    nomeEspectador.style.fontWeight = 700;
-  }
+const renderizarMensagens = mensagens => {
+  containerDoChat.innerHTML = '';
+  mensagens.forEach(mensagem => (containerDoChat.innerHTML += mensagem));
 };
 
 chat.on('message', mensagemBruta => {
   console.log({ mensagemBruta });
   inserirMensagem(mensagemBruta);
-  ordernarMensagens();
+  renderizarMensagens(mensagens);
 });
