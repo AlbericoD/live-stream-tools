16b048a0cd9213210a0ce39a39488f46d9cca5d1
AlbericoD <a.diasbarreto@icloud.com>
2019-10-11 16:52:09 -0300

    adicionado sons no momento em que alguém adivinha a palavra secreta e no momento de vitória adicionado novos estilos no nome do vencedor

--- frontend/easter-egg/index.js
+++ frontend/easter-egg/index.js
@@ -1,26 +1,32 @@
-const PONTOS_INICIAIS = 20;
+const PONTOS_INICIAIS = 1000;
 let chat = io.connect('http://localhost:8080');
-let pontosDeVida = document.querySelector('#pontos-de-vida');
+let pontosDeVidaInicial = document.querySelector('.pv-inicial');
+let pontosDeVidaAtual = document.querySelector('.pv-atual');
 let heroi = document.querySelector('.heroi');
 let buttonReset = document.querySelector('.reseta');
 let quemCausouDano = document.querySelector('.quem-causou-dano');
+let sangue = document.querySelector('.sangue');
 const desafiosData = JSON.parse(data);
-console.log(desafiosData);
-debugger;
+// console.log(desafiosData);
+// // debugger;
 
-pontosDeVida.textContent = PONTOS_INICIAIS;
+sangue.style.width = '100%';
+pontosDeVidaInicial.textContent = PONTOS_INICIAIS;
+pontosDeVidaAtual.textContent = PONTOS_INICIAIS;
 
-buttonReset.addEventListener('click', e => {
-  e.preventDefault();
-  pontosDeVida.textContent = PONTOS_INICIAIS;
-  heroi.textContent = '';
-});
+// buttonReset.addEventListener('click', e => {
+//   e.preventDefault();
+//   pontosDeVidaAtual.textContent = PONTOS_INICIAIS;
+//   heroi.textContent = '';
+//   sangue.style.width = '100%';
+// });
 
 const palavrasChaves = [
   { palavra: 'kill', pontos: 1 },
   { palavra: 'LUL', pontos: 5 },
   { palavra: '4Head', pontos: 8 },
-  { palavra: '!ban cailir', pontos: 15 }
+  { palavra: '!ban cailir', pontos: 15 },
+  { palavra: '!reddit', pontos: 989 }
 ];
 
 function pesquisaValor(palavra) {
@@ -38,19 +44,30 @@ const contabilizaPontos = palavras => {
 
 const diminuiPontos = (nome, quantidade) => {
   if (!quantidade) return;
-  let pontosAtuais = parseInt(pontosDeVida.textContent);
+  let pontosAtuais = parseInt(pontosDeVidaAtual.textContent);
   let novosPontos = Math.max(pontosAtuais - quantidade, 0);
   //aqui vou ter que identificar quem foi a pessoa que zerou os pontos de vida
   if (!pontosAtuais) return;
   if (!novosPontos) {
     heroi.textContent = `Temos um ganhador! ${nome}`;
+    let audio = new Audio('Knockout.mp3');
+    // audio.volume()
+    audio.play();
+    // sangue.style.width = '0%';
   } else {
-    quemCausouDano.textContent = `${nome} causou dano!`;
+    quemCausouDano.textContent = `${nome} causou ${quantidade} de dano!`;
+    let audio = new Audio('Pew.mp3');
+    audio.play();
     setTimeout(() => {
       quemCausouDano.textContent = '';
     }, 2000);
   }
-  pontosDeVida.textContent = novosPontos;
+
+  let pvInicial = parseInt(pontosDeVidaInicial.textContent);
+  let porcentagemAtual = parseInt(sangue.style.width);
+  let porcentagemCausada = (quantidade / pvInicial) * 100;
+  sangue.style.width = `${porcentagemAtual - porcentagemCausada}%`;
+  pontosDeVidaAtual.textContent = novosPontos;
 };
 
 const verificaPalavraChave = mensagem => {
